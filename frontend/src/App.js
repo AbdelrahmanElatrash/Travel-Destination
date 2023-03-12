ae86beebecd9fa5876530423265062c776ba72d0
AbdelrahmanElatrash <a.elatrash170@gmail.com>
2023-03-12 12:23:58 +0000

    create tour details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 
 import './App.css';
 import Home from './component/home/Home';
+import Header from './component/header/Header';
+import Footer from './component/footer/Footer'
+import {Routes, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TourDetails from './component/TourDetails/TourDetails';
+import db from "./data/db.json"
 
 function App() {
   return (
-    <div>
-      <Home/>
-    </div>
+    <>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/city/:id"  element={<TourDetails data={db}/>}></Route>
+      </Routes>
+      <Footer/>
+    </>
   );
 }
 
--- frontend/src/component/tours/Tours.js
+++ frontend/src/component/tours/Tours.js
@@ -1,18 +1,22 @@
-
+import Tour from "./tour/Tour"
+ 
 
 const Tours=(props)=>{
-    console.log(props.data)
+    
     return (
-          <div>
-            {props.data.map((item)=>{
+          <>
+          {props.data.map((item)=>{
                 return (
-                    <div key={item.ID}>
-                        <h3>{item.name}</h3>
-                        <img src={item.image} alt={item.name}></img>
-                    </div>
+                    <Tour tour={item}/>
+                    // <div key={item.ID}>
+                    //     <h3>{item.name}</h3>
+                    //     <img src={item.image} alt={item.name}></img>
+                    // </div>
                 )
             })}
-          </div>
+          </>
+            
+          
     )
 }
 
